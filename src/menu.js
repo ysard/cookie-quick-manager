4b34f0b0b8e49b3013d21ae667e559452e72a343
ysard <ysard@users.noreply.github.com>
2018-07-15 14:35:28 +0200

    Refactorisation of menu.js; Improved loading; Add translations code

--- src/menu.js
+++ src/menu.js
@@ -21,189 +21,227 @@
 // IIFE - Immediately Invoked Function Expression
 (function(mycode) {
 
+    // The global arguments are passed as parameters
     mycode(window.vAPI, window, document);
 
 }(function(vAPI, window, document) {
 
-function onError(error) {
-    // Function called when a save/remove function has failed by throwing an exception.
-    console.log({"Error removing cookies:": error});
-}
+    // The arguments are now locally scoped
+    document.addEventListener("DOMContentLoaded", function(event) {
 
-function getActiveTab() {
-  //get active tab to run an callback function.
-  //it sends to our callback an array of tab objects
-  return browser.tabs.query({currentWindow: true, active: true});
-}
+        /*********** Events attached to UI elements ***********/
 
-function createWindow(createData) {
-  // Get settings
-  let get_settings = browser.storage.local.get(["addonSize", "open_in_new_tab"]);
-  get_settings.then((items) => {
+        document.addEventListener("click", (e) => {
+            let id = e.target.id;
 
-    // Open new tab
-    if (items.open_in_new_tab !== undefined && items.open_in_new_tab === true) {
-        let new_tab = browser.tabs.create({url: createData.url});
-        new_tab.then(() => {
-            console.log("The tab has been created");
+            if (id === "search_cookie_manager") {
+                // Search cookies for a domain: Send current url
+                let createData = {
+                    type: "panel",
+                    url: "cookies.html?parent_url=" + encodeURIComponent(current_tab.url),
+                };
+                createWindow(createData);
+            }
+
+            else if (id === "simple_cookie_manager") {
+                // Just launch the addon: Send empty url
+                let createData = {
+                    type: "panel",
+                    url: "cookies.html?parent_url=",
+                };
+                createWindow(createData);
+            }
+
+            else if (id === "delete_current_cookies") {
+                // Delete all cookies for the current domain & store
+                browser.runtime.getBrowserInfo().then((browser_info) => {
+                    // TODO: reuse params from initialization: avoid getBrowserInfo() call
+                    let params = {
+                        url: current_tab.url,
+                        storeId: current_tab.cookieStoreId,
+                    }
+
+                    // Detect Firefox version:
+                    // -> firstPartyDomain argument is available on Firefox 59+=
+                    // {name: "Firefox", vendor: "Mozilla", version: "60.0.1", buildID: ""}
+                    let version = browser_info.version.split('.')[0];
+                    if (parseInt(version) >= 59)
+                        params['firstPartyDomain'] = null;
+
+                    return vAPI.delete_cookies(browser.cookies.getAll(params));
+                })
+                .then((ret) => {
+                    // Force the closing of the window
+                    window.close();
+                });
+            }
+
+            else if (id === "delete_current_localstorage") {
+                // Purge LocalStore for the current domain
+                // NOTE: subdomains will not be taken into account
+                let prom = browser.browsingData.removeLocalStorage({hostnames: [(new URL(current_tab.url)).hostname, ]});
+                prom.then((ret) => {
+                    // Force the closing of the window
+                    window.close();
+                });
+            }
+
+            else if (id === "options") {
+                // Open Options Page
+                browser.runtime.openOptionsPage();
+                // Force the closing of the window
+                window.close();
+            }
+
+            e.preventDefault();
         });
-        window.close();
-        return;
+
+        /*********** Initializations ***********/
+
+        init_ui();
+
+    }, false);
+
+    /*********** Utils ***********/
+
+    function getActiveTab() {
+        //get active tab to run an callback function.
+        //it sends to our callback an array of tab objects
+        return browser.tabs.query({currentWindow: true, active: true});
     }
 
-    // Open new window
-    let height = vAPI.optimal_window_height;
-    let width = vAPI.optimal_window_width;
+    function createWindow(createData) {
+        // Get settings
+        let get_settings = browser.storage.local.get(["addonSize", "open_in_new_tab"]);
+        get_settings.then((items) => {
 
-    // If addonSize item is in storage and if previous sizes are too small
-    // => force default values
-    // 768 is the smallest width to avoid the break of the ui
-    if (items.addonSize !== undefined && items.addonSize.width >= 768 && items.addonSize.height >= height) {
-      height = items.addonSize.height;
-      width = items.addonSize.width;
+            // Open new tab
+            if (items.open_in_new_tab !== undefined && items.open_in_new_tab === true) {
+                let new_tab = browser.tabs.create({url: createData.url});
+                new_tab.then(() => {
+                    console.log("The tab has been created");
+                });
+                window.close();
+                return;
+            }
+
+            // Open new window
+            let height = vAPI.optimal_window_height;
+            let width = vAPI.optimal_window_width;
+
+            // If addonSize item is in storage and if previous sizes are too small
+            // => force default values
+            // 768 is the smallest width to avoid the break of the ui
+            if (items.addonSize !== undefined && items.addonSize.width >= 768 && items.addonSize.height >= height) {
+            height = items.addonSize.height;
+            width = items.addonSize.width;
+            }
+            //console.log({h:height, w:width});
+            //console.log({h:items.addonSize.height, w:items.addonSize.width});
+            // TODO: why it is not ok on some computers with small resolution ?
+            createData.width = width;
+            createData.height = height;
+
+            // Create window
+            createData.url += "&type=window";
+            let new_window = browser.windows.create(createData);
+            new_window.then(() => {
+                console.log("The panel has been created");
+            });
+        });
     }
-    //console.log({h:height, w:width});
-    //console.log({h:items.addonSize.height, w:items.addonSize.width});
-    // TODO: why it is not ok on some computers with small resolution ?
-    createData.width = width;
-    createData.height = height;
 
-    // Create window
-    createData.url += "&type=window";
-    let new_window = browser.windows.create(createData);
-    new_window.then(() => {
-        console.log("The panel has been created");
-    });
-  });
-}
+    function set_translations() {
+        // Set translations after the insertion of favicons and numbers of cookies/items
+        // set_translations replace specific childNodes in a predefined position
 
-/************************************/
+        // Workaround used to speed-up the load of UI for non supported locales
+        let supported_locales = ['fr', 'de'];
+        if (supported_locales.includes(browser.i18n.getUILanguage())) {
 
-document.addEventListener("click", (e) => {
-  let id = e.target.id;
+            let i18nElements = document.querySelectorAll('[data-i18n-content]');
 
-  if (id === "search_cookie_manager") {
-      // Search cookies for a domain: Send current url
-      let createData = {
-        type: "panel",
-        url: "cookies.html?parent_url=" + encodeURIComponent(current_tab.url),
-      };
-      createWindow(createData);
-  }
+            i18nElements.forEach(function (i18nElement) {
 
-  else if (id === "simple_cookie_manager") {
-    // Just launch the addon: Send empty url
-    let createData = {
-      type: "panel",
-      url: "cookies.html?parent_url=",
-    };
-    createWindow(createData);
-  }
-
-  else if (id === "delete_current_cookies") {
-    // Delete all cookies for the current domain & store
-    browser.runtime.getBrowserInfo().then((browser_info) => {
-        // TODO: reuse params from initialization: avoid getBrowserInfo() call
-        let params = {
-            url: current_tab.url,
-            storeId: current_tab.cookieStoreId,
+                let i18nMessageName = i18nElement.getAttribute('data-i18n-content');
+                i18nElement.childNodes[1].textContent = browser.i18n.getMessage(i18nMessageName);
+            });
         }
+    }
 
-        // Detect Firefox version:
-        // -> firstPartyDomain argument is available on Firefox 59+=
-        // {name: "Firefox", vendor: "Mozilla", version: "60.0.1", buildID: ""}
-        let version = browser_info.version.split('.')[0];
-        if (parseInt(version) >= 59)
-            params['firstPartyDomain'] = null;
+    function init_ui() {
 
-        return vAPI.delete_cookies(browser.cookies.getAll(params));
-    })
-    .then((ret) => {
-        // Force the closing of the window
-        window.close();
-    });
-  }
+        // Set the searched domain and its icon
+        // Display the option to delete LocalStorage
+        // Display the number of cookies and items in LocalStorage
 
-  else if (id === "delete_current_localstorage") {
-      // Purge LocalStore for the current domain
-      // NOTE: subdomains will not be taken into account
-      let prom = browser.browsingData.removeLocalStorage({hostnames: [(new URL(current_tab.url)).hostname, ]});
-      prom.then((ret) => {
-          // Force the closing of the window
-          window.close();
-      });
-  }
+        getActiveTab().then((tabs) => {
+            // Set the global var with current tab
+            current_tab = tabs[0];
 
-  else if (id === "options") {
-      // Open Options Page
-      browser.runtime.openOptionsPage();
-      // Force the closing of the window
-      window.close();
-  }
+            // Display a shortcut to search cookies for the current domain
+            let a = document.querySelector('#search_cookie_manager');
+            // Workaround for domains without favicon
+            // no alt text to avoid the break of the ui
+            let favIconUrl = (current_tab.favIconUrl === undefined) ? "icons/icon48.png" : current_tab.favIconUrl;
+            let img = document.createElement("img");
+            img.src = favIconUrl;
+            img.className = 'favicon';
+            let content = document.createTextNode((new URL(current_tab.url)).hostname);
+            a.prepend(img);
+            a.appendChild(content);
 
-  e.preventDefault();
-});
+            // Display a shortcut to delete all cookies for the current domain & store
+            // Display a shortcut to delete LocalStorage
+            browser.runtime.getBrowserInfo().then((browser_info) => {
+                let params = {
+                    url: current_tab.url,
+                    storeId: current_tab.cookieStoreId,
+                }
 
-/************************************/
+                // Detect Firefox version:
+                // {name: "Firefox", vendor: "Mozilla", version: "60.0.1", buildID: ""}
+                let version = browser_info.version.split('.')[0];
 
-var current_tab;
+                // -> firstPartyDomain argument is available on Firefox 59+=
+                if (parseInt(version) >= 59)
+                    params['firstPartyDomain'] = null;
 
-// Set the searched domain
-getActiveTab().then((tabs) => {
-  // Set the global var with current tab
-  current_tab = tabs[0];
+                // -> LocalStorage is not available on Firefox 56
+                if (parseInt(version) >= 57) {
+                    // Display the shortcut
+                    let a = document.querySelector('#delete_current_localstorage');
+                    a.style['display'] = 'inline-block';
 
-  // Display a shortcut to search cookies for the current domain
-  let a = document.querySelector('#search_cookie_manager');
-  // Workaround for domains without favicon
-  // no alt text to avoid the break of the ui
-  let favIconUrl = (current_tab.favIconUrl === undefined) ? "icons/icon48.png" : current_tab.favIconUrl;
-  let img = document.createElement("img");
-  img.src = favIconUrl;
-  img.className = 'favicon';
-  let content = document.createTextNode('Search cookie for: ' + (new URL(current_tab.url)).hostname);
-  a.appendChild(img);
-  a.appendChild(content);
+                    // Get the number of localstorage items
+                    browser.tabs.executeScript({
+                        code: "(function (){return localStorage.length;})();"
+                    }).then((ret) => {
+                        let content = document.createTextNode(" (" + ret[0] + ")");
+                        a.appendChild(content);
+                    });
+                }
 
-  // Display a shortcut to delete all cookies for the current domain & store
-  // Display a shortcut to delete LocalStorage
-  browser.runtime.getBrowserInfo().then((browser_info) => {
-      let params = {
-          url: current_tab.url,
-          storeId: current_tab.cookieStoreId,
-      }
+                return browser.cookies.getAll(params);
+            })
+            .then((cookies) => {
+                // Display the number of cookies
+                let a = document.querySelector('#delete_current_cookies');
+                // text content is a child node, at the 4rd pos
+                let content = document.createTextNode(" (" + cookies.length + ")");
+                a.appendChild(content);
+            })
+            .then((ret) => {
+                // Set translations after the insertion of favicons and numbers of cookies/items
+                // set_translations replace specific childNodes in a predefined position
+                set_translations();
+            });
+        });
 
-      // Detect Firefox version:
-      // {name: "Firefox", vendor: "Mozilla", version: "60.0.1", buildID: ""}
-      let version = browser_info.version.split('.')[0];
+    }
 
-      // -> firstPartyDomain argument is available on Firefox 59+=
-      if (parseInt(version) >= 59)
-          params['firstPartyDomain'] = null;
+    /*********** Global variables ***********/
 
-      // -> LocalStorage is not available on Firefox 56
-      if (parseInt(version) >= 57) {
-          // Display the shortcut
-          let a = document.querySelector('#delete_current_localstorage');
-          a.style['display'] = 'inline-block';
+    var current_tab;
 
-          // Get the number of localstorage items
-          browser.tabs.executeScript({
-              code: "(function (){return localStorage.length;})();"
-          }).then((ret) => {
-              let content = document.createTextNode(" (" + ret[0] + ")");
-              a.appendChild(content);
-          });
-      }
-
-      return browser.cookies.getAll(params);
-  })
-  .then((cookies) => {
-      // Print the number of cookies
-      let a = document.querySelector('#delete_current_cookies');
-      // text content is a child node, at the 4rd pos
-      let content = document.createTextNode(" (" + cookies.length + ")");
-      a.appendChild(content);
-  });
-});
 }));
